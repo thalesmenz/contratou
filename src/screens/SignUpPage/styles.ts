ab026e4eeba321b42fe902290f49d7110e1d750c
Arthur <josearthurgomesc@gmail.com>
2024-09-09 21:33:55 -0300

    tela de cadastro

---
+++ src/screens/SignUpPage/styles.ts
@@ -0,0 +1,65 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create ({
+    Container: {
+        alignItems: "center",
+    },
+
+    LabelInput: {
+        fontSize: 16,
+        marginLeft: 12
+    },
+
+    SignUpText: { 
+        fontSize: 35,
+        marginBottom: 45,
+    },
+
+    InputStyle: {
+        marginTop: 5,
+        width: 490,
+        height: 70,
+        borderWidth: 2,
+        borderColor: "#6B6464",
+        borderRadius: 20,
+        paddingLeft: 15,
+    },
+
+    ButtonTerm: {
+        color: "#5992E8",
+        fontSize: 18,
+        marginLeft: 5,
+    },
+
+    SecondButtonTerm: {
+        color: "#5992E8",
+        fontSize: 18,
+        marginLeft: 5,
+    },
+
+    TextDefault: {
+        fontSize: 18,
+        marginLeft: 5
+    },
+
+    ButtonSignUp: {
+        backgroundColor: "#5992E8",
+        width: 490,
+        height: 70,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    TextLogin: {
+        color:  "white",
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+
+    ViewLoginBottom: {
+        position: "absolute",
+        top: 790,
+        alignItems: "center"
+    },
+})
